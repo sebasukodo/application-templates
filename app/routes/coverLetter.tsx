37094a7237316dbdb433fc956ae4d7593041260a
sebasukodo <231564377+sebasukodo@users.noreply.github.com>
2026-06-04 11:30:29 +0200

    changing webpage titles

--- app/routes/coverLetter.tsx
+++ app/routes/coverLetter.tsx
@@ -1,8 +1,9 @@
 import type { Route } from "./+types/coverLetter";
 import CoverLetter from "../pages/german/coverLetter";
+import { application } from "../userInfo";
 
 export function meta({}: Route.MetaArgs) {
-  return [{ title: "Cover Letter" }];
+  return [{ title: `${application.userInfo.name} - Anschreiben` }];
 }
 
 export default function ApplicationPage() {
--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,8 +1,9 @@
 import type { Route } from "./+types/resume";
 import Resume from "../pages/german/resume";
+import { application } from "../userInfo";
 
 export function meta({}: Route.MetaArgs) {
-  return [{ title: "Resume" }];
+  return [{ title: `${application.userInfo.name} - Lebenslauf` }];
 }
 
 export default function ResumePage() {
